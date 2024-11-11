e6fc29bf56a774e8d17268e561cd9fb9e256537b
신동근 <tlsehdrms124@naver.com>
2024-11-11 16:26:13 +0900

    [BE] feat: 회원 권한 매핑 엔티티 정의

---
+++ BackEnd/src/main/java/com/example/backend/domain/MemberRoleMapping.java
@@ -0,0 +1,21 @@
+package com.example.backend.domain;
+
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberRoleMapping {
+
+    private UUID id;
+
+    private Member member;
+
+    private Role role;
+}
